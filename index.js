1ed1b5dd4917318bcba9b1cf0a42896383a65f28
Moise-code <moisewowe@gmail.com>
2023-01-24 06:12:19 -0800

    adding the modal and the close modal functionality

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const addMovieModal = document.querySelector('#add-modal');
 const addBackdrop = document.querySelector('#backdrop')
 const startAddMovieButton = document.querySelector('header button');
+const cancelModal = document.querySelector('.btn--passive');
 
 
 const toggleMovieModal = () =>{
   // function to add the modal after clicking the add movie button.
  addMovieModal.classList.toggle('visible');
  toggleBackdrop();
-
+ 
 }
+
+const closeModal = () =>{
+addBackdrop.classList.remove('visible');
+  addMovieModal.classList.remove('visible');
+}
+cancelModal.addEventListener('click' , () =>{
+  closeModal();
+  
+})
+addBackdrop.addEventListener('click', ()=>{
+  closeModal();
+})
 
 const toggleBackdrop = () =>{
   addBackdrop.classList.toggle('visible');
