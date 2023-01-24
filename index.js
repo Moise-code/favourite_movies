4ada3c8963b0a672abd912280afe1387696be286
Moise-code <moisewowe@gmail.com>
2023-01-24 06:34:14 -0800

    adding the simple inputes validation of the form modal

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const addMovieModal = document.querySelector('#add-modal');
 const addBackdrop = document.querySelector('#backdrop')
 const startAddMovieButton = document.querySelector('header button');
 const cancelModal = document.querySelector('.btn--passive');
+const confirmAddMovieButton = document.querySelector('.btn--success');
+const userInputes = addMovieModal.querySelectorAll('input');
+
 
 
 const toggleMovieModal = () =>{
@@ -19,7 +22,22 @@ addBackdrop.classList.remove('visible');
 const toggleBackdrop = () =>{
   addBackdrop.classList.toggle('visible');
 }
+const addMovieHandler = () =>{
+
+  const titleMovie = userInputes[0].value;
+  const imageUrlValue = userInputes[1].value;
+  const ratingValue = userInputes[2].value;
+  if(titleMovie.trim() === '' ||
+     imageUrlValue.trim() === '' ||
+     ratingValue.trim() === '' ||
+     +ratingValue < 1 || 
+     +ratingValue > 5){
+      alert('please use valid information and rating from 1 to 5');
+      userInputes.value = '';
+     }
+
+}
 startAddMovieButton.addEventListener('click',toggleMovieModal);
 cancelModal.addEventListener('click' , closeModal);
 addBackdrop.addEventListener('click',closeModal);
-
+confirmAddMovieButton.addEventListener('click', addMovieHandler)
