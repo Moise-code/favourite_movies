f2999875560727147f362f3503d45968b8130c24
Moise-code <moisewowe@gmail.com>
2023-01-24 06:01:25 -0800

    adding the functionality to bring the modal once click the add movie button

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const 
+const addMovieModal = document.querySelector('#add-modal');
+const addBackdrop = document.querySelector('#backdrop')
+const startAddMovieButton = document.querySelector('header button');
+
+
+const toggleMovieModal = () =>{
+  // function to add the modal after clicking the add movie button.
+ addMovieModal.classList.toggle('visible');
+ toggleBackdrop();
+
+}
+
+const toggleBackdrop = () =>{
+  addBackdrop.classList.toggle('visible');
+}
+startAddMovieButton.addEventListener('click',toggleMovieModal);
+
