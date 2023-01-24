4e70ce7e52ec52e90e92176feab480779f4d9b00
Moise-code <moisewowe@gmail.com>
2023-01-24 06:44:53 -0800

    add functionality to clear the inputs after adding the movie

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cancelModal = document.querySelector('.btn--passive');
 const confirmAddMovieButton = document.querySelector('.btn--success');
 const userInputes = addMovieModal.querySelectorAll('input');
 
-
+const movies = [];
 
 const toggleMovieModal = () =>{
   // function to add the modal after clicking the add movie button.
@@ -22,6 +22,11 @@ addBackdrop.classList.remove('visible');
 const toggleBackdrop = () =>{
   addBackdrop.classList.toggle('visible');
 }
+const clearUserInput = () =>{
+  for (const usrInput of userInputes ) {
+    usrInput.value = '';
+  } 
+}
 const addMovieHandler = () =>{
 
   const titleMovie = userInputes[0].value;
@@ -33,9 +38,19 @@ const addMovieHandler = () =>{
      +ratingValue < 1 || 
      +ratingValue > 5){
       alert('please use valid information and rating from 1 to 5');
-      userInputes.value = '';
+     } 
+
+     // creating the object to get the input values
+     const newMovie = {
+      title : titleMovie,
+      image : imageUrlValue,
+      rating : ratingValue
      }
 
+     movies.push(newMovie);
+     console.log(movies);
+     clearUserInput();
+     closeModal();
 }
 startAddMovieButton.addEventListener('click',toggleMovieModal);
 cancelModal.addEventListener('click' , closeModal);
